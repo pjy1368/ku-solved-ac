9ea438b3b10c9db6fa25ab3942bc1e644ffa1be0
JinYoung Park <basejin0810@naver.com>
2021-06-02 10:17:42 +0900

    init: 초기 설정

---
+++ src/main/java/com/konkuk/solvedac/SolvedacApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.solvedac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvedacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvedacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/solvedac/SolvedacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.solvedac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvedacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
