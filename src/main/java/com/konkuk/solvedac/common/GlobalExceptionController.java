85be752591a2792ebe4de573d3917348c1d31297
JinYoung Park <basejin0810@naver.com>
2021-06-12 13:00:40 +0900

    feat: Logger 구현

--- src/main/java/com/konkuk/solvedac/common/GlobalExceptionController.java
+++ src/main/java/com/konkuk/solvedac/common/GlobalExceptionController.java
@@ -3,6 +3,8 @@ package com.konkuk.solvedac.common;
 import com.konkuk.solvedac.exception.InvalidInputException;
 import com.konkuk.solvedac.exception.NotFoundException;
 import javax.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,23 +13,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionController {
 
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> unConstraintViolationHandler(ConstraintViolationException exception) {
+        fileLogger.error(exception.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage().split(":")[1].strip()));
     }
 
     @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<ErrorResponse> invalidInputExceptionHandler(InvalidInputException exception) {
+        fileLogger.error(exception.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> notFoundExceptionHandler(NotFoundException exception) {
+        fileLogger.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> serverExceptionHandler(Exception exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
+        fileLogger.error(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse("앗! 예상치 못한 에러가 발생했습니다. 관리자에게 문의바랍니다."));
     }
 }
