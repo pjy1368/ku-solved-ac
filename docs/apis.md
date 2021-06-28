9fb689eced66829e9291ac875871a15b18460f23
JinYoung Park <basejin0810@naver.com>
2021-06-28 13:14:02 +0900

    docs(apis.md): 업데이트된 api 재수정

--- docs/apis.md
+++ docs/apis.md
@@ -1,5 +1,5 @@
 ## 2021.06.19 이후로 수정된 api 정리
-1. https://solved.ac/api/v3/ranking/organization/{group_id}
+1. https://solved.ac/api/v3/ranking/in_organization?organizationId={group_id}
 - 특정 그룹의 유저 목록 가져오는 api.
 
 2. https://solved.ac/api/v3/search/problem?query=solved_by:{user_id}
@@ -13,3 +13,4 @@
 - 티어는 unrated, b5, b4, ... , r1까지 있음.
 
 주의) '&page=' 쿼리를 통해 페이지를 구분해야 함.
+또한, 업데이트 이후 총 page 개수를 반환하지 않는데 한 페이지당 100개인 것을 알아둘 것.
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,29 +1,33 @@
-drop table if exists PROBLEM;
-drop table if exists USER;
 drop table if exists USER_PROBLEM_MAP;
-drop table if exists TEMP_PROBLEM;
-drop table if exists TEMP_USER;
+drop table if exists USER;
+drop table if exists PROBLEM;
 drop table if exists TEMP_USER_PROBLEM_MAP;
+drop table if exists TEMP_USER;
+drop table if exists TEMP_PROBLEM;
 
 create table if not exists PROBLEM
 (
     id           bigint       not null unique,
     level        int          not null,
     title        varchar(255) not null,
-    solved_count int          not null
+    solved_count int          not null,
+    primary key (id)
 );
 
 create table if not exists USER
 (
     id       varchar(255) not null unique,
-    group_id bigint
+    group_id bigint,
+    primary key (id)
 );
 
 create table if not exists USER_PROBLEM_MAP
 (
     user_id    varchar(255) not null,
     group_id   bigint,
-    problem_id bigint       not null
+    problem_id bigint       not null,
+    foreign key (user_id) references USER (id),
+    foreign key (problem_id) references PROBLEM (id)
 );
 
 create table if not exists TEMP_PROBLEM
@@ -31,18 +35,22 @@ create table if not exists TEMP_PROBLEM
     id           bigint       not null unique,
     level        int          not null,
     title        varchar(255) not null,
-    solved_count int          not null
+    solved_count int          not null,
+    primary key (id)
 );
 
 create table if not exists TEMP_USER
 (
     id       varchar(255) not null unique,
-    group_id bigint
+    group_id bigint,
+    primary key (id)
 );
 
 create table if not exists TEMP_USER_PROBLEM_MAP
 (
     user_id    varchar(255) not null,
     group_id   bigint,
-    problem_id bigint       not null
+    problem_id bigint       not null,
+    foreign key (user_id) references TEMP_USER (id),
+    foreign key (problem_id) references TEMP_PROBLEM (id)
 );
